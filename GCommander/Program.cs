97d4e82b49793bb76f9af37f7002711b6d1990fc
Uwe Riegel <uriegel@hotmail.de>
2026-07-15 19:52:18 +0200

    WebSiteFromResource

--- GCommander/Program.cs
+++ GCommander/Program.cs
@@ -4,6 +4,7 @@ Application
     .NewAdwaita("de.uriegel.gcommander")
     .WithDiagnostics(true)
     .WithWebKit()
+    .WithWebsiteFromResource()
     .WithSettings()
     .OnActivate(app => app
         .WindowFromBuilder("mainwindow", "window", p => new MainWindow(p))
--- GCommander/UI/LocationViewer.cs
+++ GCommander/UI/LocationViewer.cs
@@ -7,8 +7,6 @@ class LocationViewer : Box
     public LocationViewer(Builder builder, string name)
         : base(builder, name)
     {
-        WebKitWebContext.GetDefault().RegisterUriScheme("res", OnResRequest);
-        OnFinalize(WebKitWebContext.DisposeUriSchemes);
         locationView.LoadUri("res://location/index.html");
 
         this["visible"].OnNotify += () =>
@@ -40,45 +38,6 @@ class LocationViewer : Box
                     locationView.Visible = false;
             }
         };
-    }
-
-    static void OnResRequest(WebkitUriSchemeRequest request)
-    {
-        try
-        {
-            var uri = request.GetUri()[6..].SubstringUntil('?');
-            uri = uri.Length > 0 ? uri : "index.html";
-            var res = Resources.Get(uri);
-            if (res != null)
-            {
-                var bytes = new byte[res.Length];
-                var read = res.Read(bytes, 0, bytes.Length);
-                using var gbytes = GBytes.New(bytes);
-                using var gstream = MemoryInputStream.New(gbytes);
-                request.Finish(gstream, bytes.Length, uri?.GetFileExtension()?.ToMimeType() ?? "text/html");
-            }
-            else
-                SendNotFound(request);
-        }
-        catch
-        {
-            SendNotFound(request);
-        }
-    }
-
-    static void SendNotFound(WebkitUriSchemeRequest request)
-        => SendResponse(request, 404, "Not Found", "I can't find what you're looking for!");
-
-    static void SendResponse(WebkitUriSchemeRequest request, int code, string status, string text)
-    {
-        using var bytes = GBytes.New(Encoding.UTF8.GetBytes(text));
-        using var stream = MemoryInputStream.New(bytes);
-        using var response = WebKitUriSchemeResponse.New(stream, text.Length);
-        using var respondHeaders = SoupMessageHeaders.New(SoupMessageHeaderType.Response);
-        respondHeaders.Set([new("Access-Control-Allow-Origin", "*")]);
-        response.HttpHeaders(respondHeaders);
-        response.Status(code, status);
-        request.Finish(response);
     }
 
     [Widget]
